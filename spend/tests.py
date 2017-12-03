742d8f2327cc240753a446044ef7561a9bdce825
Roger Walker <roger.john.walker@gmail.com>
2017-12-03 12:36:44 -0500

    Model for expense items, and the db migration

--- spend/tests.py
+++ spend/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from spend.views import home_page
+from spend.models import Expense
 
 class HomePageTest(TestCase):
 	
@@ -19,4 +20,22 @@ class HomePageTest(TestCase):
 		self.assertIn('A new expense', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
 		
-	
+
+class ExpenseModelTest(TestCase):
+
+	def test_saving_and_retreiving_expenses(self):
+		first_expense = Expense()
+		first_expense.text = 'The first (ever) expense!'
+		first_expense.save()
+		
+		second_expense = Expense()
+		second_expense.text = 'The Second'
+		second_expense.save()
+		
+		saved_items = Expense.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+		
+		first_saved_expense = saved_items[0]
+		second_saved_expense = saved_items[1]
+		self.assertEqual(first_saved_expense.text, 'The first (ever) expense!')
+		self.assertEqual(second_saved_expense.text, 'The Second')
