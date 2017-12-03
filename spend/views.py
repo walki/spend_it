39279cff121b9bfd7acd8d27b5b32719c8190468
Roger Walker <roger.john.walker@gmail.com>
2017-12-02 21:37:48 -0500

    Refactor to templated homepage!

--- spend/views.py
+++ spend/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-	return HttpResponse('<html><title>Spend It!</title></html>')
+	return render(request, 'home.html')
