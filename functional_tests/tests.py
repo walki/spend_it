44dc4d565a2ae2f050642546131f4fae557074d4
Roger Walker <roger.john.walker@gmail.com>
2017-12-07 21:12:42 -0500

    Added FT to support second user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -28,7 +28,7 @@ class NewVisitorTest(LiveServerTestCase):
 					raise e
 				time.sleep(0.5)
 		
-	def test_can_start_adding_expenses_and_retrieve_later(self):
+	def test_can_start_adding_expenses_for_one_user(self):
 		# Andrea wants to help with the budget using a cool new expesne tracking software
 		# She goes to its homepage 
 		self.browser.get(self.live_server_url)
@@ -72,14 +72,54 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys(Keys.ENTER)
 
 		# The page updates again, and both expenses are listed
-		self.wait_for_row_in_expense_table('1: 1-1-2018 Heinens 10.00')
 		self.wait_for_row_in_expense_table('2: 2-1-2018 Target 10.00')		
-		
-		# Andrea wonders whether the expenses are saved. She notices that in th first go through, expense are added per unique URL,
-		# there is some text explaining that
-		self.fail('Finish the test!')
-		
-		# She visits the URL and her expenses are still there
+		self.wait_for_row_in_expense_table('1: 1-1-2018 Heinens 10.00')
+				
+	
+	def test_multiple_users_can_start_lists_at_differenty_urls(self):
+		# Andrea starts a new expenses list
+		self.browser.get(self.live_server_url)
+		inputbox = self.browser.find_element_by_id('id_new_expense')
+		inputbox.send_keys('Giant Eagle')
+		dateinputbox = self.browser.find_element_by_id('id_new_date')
+		dateinputbox.send_keys('12-12-2017')
+		inputbox.send_keys(Keys.ENTER)
 
-		# Satisfied, she leave
+		self.wait_for_row_in_expense_table('1: 12-12-2017 Giant Eagle 10.00')
+		
+		# She notices that her list has a unique URL
+		andrea_list_url = self.browser.current_url
+		self.assertRegex(andrea_list_url, '/spends/.+')
+		
+		# Now another user comes along, Anna ...
+		
+		## We use a new browser session, no info from the cookies...
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+		
+		# Anna visits the home page, and there is no sign of Andrea's list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Giant Eagle', page_text)
+		self.assertNotIn('Heinens', page_text)
+		
+		# Anna starts her own list of expenses
+		inputbox = self.browser.find_element_by_id('id_new_expense')
+		inputbox.send_keys('Jo Ann Fabrics')
+		dateinputbox = self.browser.find_element_by_id('id_new_date')
+		dateinputbox.send_keys('12-28-2017')
+		inputbox.send_keys(Keys.ENTER)
+		self.wait_for_row_in_expense_table('1: 12-28-2017 Jo Ann Fabrics 10.00')
+		
+		#Anna gets her own unique URL
+		anna_list_url = self.browser.current_url
+		self.assertRegex(anna_list_url, '/spends/.+')
+		self.assertNotEqual(anna_list_url, andrea_list_url)
+		
+		# And there is still no trace of Andrea's items
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Giant Eagle', page_text)
+		self.assertIn('Jo Ann Fabrics', page_text)
+		
+		# Satisfied, they both go back to sleep...
 		
